4e163ec3450a50c55887c69302079e636bf34790
Ismael_Henrique <Ismael_Henrique@outlook.pt>
2024-04-27 13:41:35 -0300

    remove when checked bug checkbox resolved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,38 @@ export const Home = () => {
   const [search, setSearch] = useState('')
   const [openModal, setOpenModal] = useState(false)
 
-  const filteredProducts:any = products.filter((product) => product.title.toLocaleLowerCase().includes(search))
+  const [total, setTotal] = useState(0)
+  const [quanty, setQuanty] = useState(0)
 
-  //Trabalhando aqui
+  const filteredProducts:any = products.filter((product) => product.title.toLocaleLowerCase().includes(search))
 
   const [cart, setCart] = useState<any>(() => {
     const storedData = localStorage.getItem("@product")
     return storedData ? JSON.parse(storedData) : []
   })
+
+  const [isCheckedArray, setIsCheckedArray] = useState(Array(cart.length).fill(false))
+
+  const handleCheckboxChange = (index: number, isChecked?: boolean) => {
+
+    const newCheckedArray = [...isCheckedArray]
+    newCheckedArray[index] = isChecked
+    setIsCheckedArray(newCheckedArray)
+
+    let newTotal = 0
+    let newQuanty = 0
+
+    newCheckedArray.forEach((isChecked, index) => {
+      if (isChecked) {
+        newTotal += cart[index].price
+        newQuanty++
+      }
+    })
+    setTotal(newTotal)
+    setQuanty(newQuanty)
+  }
+
+  //Functions cart
   
   const addToCart = (product:any) => {
     const newProduct = [...cart, product]
@@ -29,35 +53,19 @@ export const Home = () => {
     cartUpdated.splice(index, 1)
     localStorage.setItem("@product", JSON.stringify(cartUpdated))
     setCart(cartUpdated)
+    handleCheckboxChange(index)
   }
 
   const removeAllProducts = () => {
     localStorage.clear()
     setCart([])
+    setTotal(0)
+    setQuanty(0)
   }
 
-  const [total, setTotal] = useState(0)
-  const [quanty, setQuanty] = useState(0)
+  //Checkbox/total configuration
 
-  const [isCheckedArray, setIsCheckedArray] = useState(Array(cart.length).fill(false))
-
-  const handleCheckboxChange = (index: number, isChecked: boolean) => {
-
-    const newCheckedArray = [...isCheckedArray]
-    newCheckedArray[index] = isChecked
-    setIsCheckedArray(newCheckedArray)
-
-    let newTotal = 0
-    let newQuanty = 0
-    newCheckedArray.forEach((isChecked, index) => {
-      if (isChecked) {
-        newTotal += cart[index].price
-        newQuanty++
-      }
-    })
-    setTotal(newTotal)
-    setQuanty(newQuanty)
-  }
+ 
 
   return (
     <>
